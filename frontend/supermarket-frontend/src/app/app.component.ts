111309336eeb712fbf9a0a121610d05239bf9011
Leand3k <theleand3000@gmail.com>
2022-08-11 01:56:11 -0400

    Docker file added.

--- frontend/supermarket-frontend/src/app/app.component.ts
+++ frontend/supermarket-frontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RESTService } from 'src/app/rest.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'supermarket-frontend';
+
+  constructor(private router: Router) {}
+
+  productsList() {
+    this.router.navigate(['products']);
+  }
+
+  createProduct() {
+    this.router.navigate(['create']);
+  }
 }
